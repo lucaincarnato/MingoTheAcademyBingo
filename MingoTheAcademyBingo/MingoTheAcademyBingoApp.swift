8dd3810fb80b2662cd9f985fad7bb382c11b8e54
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-08 11:25:43 +0100

    Initial Commit

---
+++ MingoTheAcademyBingo/MingoTheAcademyBingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MingoTheAcademyBingoApp.swift
+//  MingoTheAcademyBingo
+//
+//  Created by Luca Maria Incarnato on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MingoTheAcademyBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
