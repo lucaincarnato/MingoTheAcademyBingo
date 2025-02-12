0deee6fd6dd826d44f304f716a0a674747847bee
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-12 12:32:47 +0100

    UI final touches

--- MingoTheAcademyBingo/ContentView.swift
+++ MingoTheAcademyBingo/ContentView.swift
@@ -9,22 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State var scorecard: Scorecard = Scorecard()
+    @State var won: Bool = false
     
     var body: some View {
         ZStack{
             Color.red
                 .ignoresSafeArea()
             VStack {
-                HStack {
-                    Text("Mingo")
-                        .bold()
-                        .font(.system(size: 70))
-                    Text("The academy Bingo")
-                        .frame(width: 100, alignment: .center)
-                        .font(.system(size: 25))
-                }
-                ScorecardView(scorecard: $scorecard)
+                Image("title")
+                    .resizable()
+                    .scaledToFit()
+                ScorecardView(scorecard: $scorecard, won: $won)
             }
+        }
+        .sheet(isPresented: $won) {
+            Text("YOU WON")
         }
     }
 }
--- MingoTheAcademyBingo/ScorecardView.swift
+++ MingoTheAcademyBingo/ScorecardView.swift
@@ -9,30 +9,38 @@ import SwiftUI
 
 struct ScorecardView: View {
     @Binding var scorecard: Scorecard
+    @Binding var won: Bool
     
+    let cellSize = UIScreen.main.bounds.width / 4
     let rows = [
-        GridItem(.fixed(30), spacing: 35),
-        GridItem(.fixed(30), spacing: 35),
-        GridItem(.fixed(30), spacing: 35),
-        GridItem(.fixed(30), spacing: 35),
+        GridItem(.fixed(1.4137 * UIScreen.main.bounds.width / 4), spacing: 0),
+        GridItem(.fixed(1.4137 * UIScreen.main.bounds.width / 4), spacing: 0),
+        GridItem(.fixed(1.4137 * UIScreen.main.bounds.width / 4), spacing: 0),
+        GridItem(.fixed(1.4137 * UIScreen.main.bounds.width / 4), spacing: 0)
     ]
     
     var body: some View {
-        VStack{
+        VStack {
             LazyHGrid(rows: rows, spacing: 0) {
                 ForEach(scorecard.phrases) { phrase in
-                    Button(){
-                        phrase.status.toggle()
+                    Button {
+                        phrase.status = true
+                        won = scorecard.bingo()
                     } label: {
-                        Text(phrase.text)
-                            .font(.title2)
-                            .bold()
-                            .frame(width: 50, height: 50)
+                        ZStack {
+                            Image("placeholder")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: cellSize, height: cellSize)
+                            
+                            Color.green.opacity(phrase.status ? 0.5 : 0)
+                        }
                     }
-                    .buttonStyle(.bordered)
-                    .foregroundStyle(Color.black)
+                    .frame(width: cellSize, height: cellSize)
                 }
             }
+            .frame(maxWidth: .infinity, alignment: .center)
         }
+        .frame(maxWidth: .infinity, alignment: .center)
     }
 }
